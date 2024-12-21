d495e1de6dc9f06f77495efb93214952632a523f
Ender Shiru <endershiru@outlook.com>
2024-12-21 13:03:22 -0800

    mmmm
    Signed-off-by: Ender Shiru <endershiru@outlook.com>

--- scripts/config.sh
+++ scripts/config.sh
@@ -247,7 +247,7 @@ function format() {
 	local type="${arguments[type]}"
 	if [[ "$type" == "btrfs" ]]; then
 		USED_BTRFS=true
-	elif [[ "$type" == "xfs"]] then
+	elif [[ "$type" == "xfs"]];
 		USED_XFS=true
 	fi
 
